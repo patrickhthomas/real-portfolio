73589dda41bf6bbab3a8fdc5fa12f4f59dbbd72a
Patrick <patrick@sendhelp.biz>
2020-11-30 13:08:22 -0500

    Polyfill install

--- js/main.js
+++ js/main.js
@@ -1,38 +1,70 @@
+
+
+
 const config = {
   // Add root here so rootBounds in entry object is not null
   root: document,
   // Margin to when element should take action
-  rootMargin: '-70px',
+  rootMargin: '0%',
   // Fine tune threshold. The callback will fired 30 times during intersection. You can change it to any number yout want
-  threshold: [...Array(4).keys()].map(x => x / 3)
+  threshold: [...Array(24).keys()].map(x => x / 48)
 };
 
 let observer = new IntersectionObserver(function(entries, observer) {
 
   entries.forEach((entry, index) => {
-    const element = entry.target;
+    var element = entry.target;
 
+    var viewportOffset = entry.getBoundingClientRect();
     // Get root elemenet (document) coords
-    const rootTop = entry.rootBounds.top;
-    const rootBottom = entry.rootBounds.height;
+    var margin = entry.intersectionRatio;
+    var top = viewportOffset.top;
 
     // Get div coords
-    const topBound = entry.boundingClientRect.top + 50; // -50 to count for the margine in config
-    const bottomBound = entry.boundingClientRect.bottom;
+    var divTop = entry.rootBounds.top; // -50 to count for the margine in config
+    var divBottom = entry.rootBounds.bottom;
 
     let className;
 
+
     // Do calculations to get class names
-    if ((topBound) < rootTop && bottomBound < rootTop) {
+    if (margin > 0.3 && divBottom < margin) {
       className = "outview-top";
-    } else if ((topBound) > rootBottom) {
+    console.log ("current margin is" + " " + entry.intersectionRatio);
+    console.log ("current offset is" + " " + window.pageYOffset);
+    console.log(divTop);
+    console.log(divBottom);
+
+    } else if ((divTop) > top) {
       className = "outview-bottom";
-    } else if ((topBound + 0) < rootBottom && bottomBound > rootBottom) {
+    console.log ("current margin is" + " " + entry.intersectionRatio);
+    console.log ("current offset is" + " " + window.pageYOffset);
+    console.log(divTop);
+    console.log(divBottom);
+
+    } else if ((divTop + 0) < top && divBottom > top) {
       className = "inview-top";
-    } else if ((topBound + 0) < rootTop && bottomBound > rootTop) {
+    console.log ("current margin is" + " " + entry.intersectionRatio);
+    console.log ("current offset is" + " " + window.pageYOffset);
+    console.log(divTop);
+    console.log(divBottom);
+
+    } else if ((divTop + 0) < margin && divBottom > margin) {
       className = "inview-bottom";
+    console.log ("current margin is" + " " + entry.intersectionRatio);
+    console.log ("current offset is" + " " + window.pageYOffset);
+    console.log(divTop);
+    console.log(divBottom);
+
     }
     element.setAttribute('data-view', className);
+
+    console.log ("current margin is" + " " + entry.intersectionRatio);
+    console.log ("current offset is" + " " + window.pageYOffset);
+    console.log(divTop);
+    console.log(divBottom);
+
+
 
   })
 }, config);
@@ -40,7 +72,12 @@ let observer = new IntersectionObserver(function(entries, observer) {
 const viewbox = document.querySelectorAll('.border');
 viewbox.forEach(image => {
   observer.observe(image);
+  config;
 });
+
+
+    
+
 
 
 
